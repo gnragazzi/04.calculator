2147cdd25fda0bde162248d1aef1408178f17481
gnragazzi <gnragazzi@gmail.com>
2021-03-25 14:18:53 -0300

    Created all elements and data array

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from 'react'
+import data from './data'
 
 function App() {
   return (
-    <div>
-      <h1>hello</h1>
-    </div>
+    <main>
+      <article className='calculator'>
+        <div className='display' id='display'>
+          <p className='display-result'>0</p>
+        </div>
+        <div className='calc-container'>
+          {data.map((item) => {
+            return (
+              <button className='calc-btn' id={item.name}>
+                {item.symbol}
+              </button>
+            )
+          })}
+        </div>
+      </article>
+    </main>
   )
 }
 
---
+++ src/data.js
@@ -0,0 +1,70 @@
+export default [
+  {
+    name: 'add',
+    symbol: '+',
+  },
+  {
+    name: 'subtract',
+    symbol: '-',
+  },
+  {
+    name: 'multiply',
+    symbol: '*',
+  },
+  {
+    name: 'divide',
+    symbol: '/',
+  },
+  {
+    name: 'equals',
+    symbol: '=',
+  },
+  {
+    name: 'clear',
+    symbol: 'C',
+  },
+  {
+    name: 'one',
+    symbol: '1',
+  },
+  {
+    name: 'two',
+    symbol: '2',
+  },
+  {
+    name: 'three',
+    symbol: '3',
+  },
+  {
+    name: 'four',
+    symbol: '4',
+  },
+  {
+    name: 'five',
+    symbol: '5',
+  },
+  {
+    name: 'six',
+    symbol: '6',
+  },
+  {
+    name: 'seven',
+    symbol: '7',
+  },
+  {
+    name: 'eight',
+    symbol: '8',
+  },
+  {
+    name: 'nine',
+    symbol: '9',
+  },
+  {
+    name: 'zero',
+    symbol: '0',
+  },
+  {
+    name: 'decimal',
+    symbol: '.',
+  },
+]
